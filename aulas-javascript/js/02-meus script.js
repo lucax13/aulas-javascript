b46737b6cf67eda4d000a1cbedbf37668ec12e4b
lucax13 <lucas13lima2006@gmail.com>
2024-09-30 11:51:54 -0300

    inicio de java com arrays

---
+++ aulas-javascript/js/02-meus script.js
@@ -0,0 +1,5 @@
+console.log("meu arquivo javascript");
+let uc = 3;
+let nome = "codificar front-end";
+
+console.log(`estamos na uc ${uc} estudando somo ${nome}`);
---
+++ aulas-javascript/js/03-arrays.js
@@ -0,0 +1,10 @@
+
+
+// declaraçao array
+let alunos =["noah", "pietro", "lucas"];
+
+// estrutura array
+console.log(alunos);
+
+// elementos especificos do array
+console.log(` ${alunos[2]} e o vocalista da banda👻`);
